2e0080e3360f2d0fec476007cecd7e0b48aa21c8
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-10 15:04:26 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,6 @@
-#!/bin/bash
-#!/bin/bash
-echo '#include <stdio.h>' > temp.c && echo 'int main(void) { printf("Programming is like building a multilingual puzzle\\n"); return 0; }' >> temp.c && gcc -Wall -Werror -Wextra -pedantic -std=gnu89 temp.c -o cisfun && ./cisfun
+#include <stdio.h>
+
+int main(void) {
+  puts("Programming is like building a multilingual puzzle");
+  return 0;
+}
