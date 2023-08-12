d00011e16e305ce04ab896022fc5ada88afbd06a
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-12 13:30:11 -0700

    correction

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single-digit numbers of base 10 starting from 0.
+ * Description: Prints all single-digit numbers of base 10.
  *
  * Return: Always 0 (Success)
  */
