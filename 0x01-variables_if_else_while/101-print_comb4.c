fdad6ea5e88394f3549dde427bfecbf93332eaf1
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-12 14:06:54 -0700

    correction

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,25 +3,29 @@
 /**
  * main - Entry point
  *
- * Description: Prints all possible different combinations of two digits.
+ * Description: Prints all possible different combinations of three digits.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int digit1, digit2;
+	int digit1, digit2, digit3;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (digit2 = digit1 + 1; digit2 <= 9; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			putchar(digit1 + '0');
-			putchar(digit2 + '0');
-
-			if (digit1 != 8 || digit2 != 9)
+			for (digit3 = digit2 + 1; digit3 <= 9; digit3++)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(digit1 + '0');
+				putchar(digit2 + '0');
+				putchar(digit3 + '0');
+
+				if (digit1 != 7 || digit2 != 8 || digit3 != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
