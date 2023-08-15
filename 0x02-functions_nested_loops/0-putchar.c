83fc812526ecc1c7c8c9f7d9fa42427feaa2f1a9
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-15 16:15:53 -0700

    corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
+#include "main.h"
+
+void _putchar(char c) {
+  putchar(c);
+}
+
+/**
+ *this is project
+ *is a hard project
+ *but I will try
+ */
 int main() {
-  putchar('_');
-  putchar('p');
-  putchar('u');
-  putchar('t');
-  putchar('c');
-  putchar('h');
-  putchar('\n');
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('\n');
+
   return 0;
 }
