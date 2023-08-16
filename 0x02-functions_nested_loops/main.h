3e63d6e616e7a8b0b3e3b5aafc207ed0c04eae69
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-16 06:28:53 -0700

    changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -60,5 +60,24 @@ int _putchar(char c);
  */
 int _islower(int c);
 
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ *
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the number of characters written.
+ *         On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _isalpha - Checks if a character is an alphabetic character.
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is an alphabetic character, 0 otherwise.
+ */
+int _isalpha(int c);
+
 #endif /* MAIN_H */
 
