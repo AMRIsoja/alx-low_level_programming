09f5fbeb21355e517628ffc465999ca4f251e658
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-15 20:35:02 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/main_2.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ *
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the number of characters written.
+ *         On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase, ten times.
+ */
+void print_alphabet_x10(void);
+
+#endif /* MAIN_H */
