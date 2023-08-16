9ad1d008a615adb7197aca15d2bb3a3a11f73d7f
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-16 05:15:56 -0700

    changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -26,5 +26,20 @@ int _putchar(char c);
  */
 void print_alphabet(void);
 
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ *
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the number of characters written.
+ *         On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase, ten times.
+ */
+void print_alphabet_x10(void)
+
 #endif /* MAIN_H */
 
