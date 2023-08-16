f984db5d34e9efcdb979d1831cd93af0925235b0
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-16 06:39:53 -0700

    changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -79,5 +79,16 @@ int _putchar(char c);
  */
 int _isalpha(int c);
 
+/**
+ * print_sign - Prints the sign of a number.
+ *
+ * @n: The number to be checked.
+ *
+ * Return: 1 and prints + if n is greater than zero,
+ *         0 and prints 0 if n is zero,
+ *        -1 and prints - if n is less than zero.
+ */
+int print_sign(int n);
+
 #endif /* MAIN_H */
 
