b8180ecf3eecc8f561158003d82802a0bd021cd3
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-16 11:05:11 -0700

    corrections

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,17 @@ void times_table(void)
 	int row, col, product;
 
 	for (row = 0; row <= 9; row++)
-	{
+        {
 		for (col = 0; col <= 9; col++)
-		{
+                {
 			product = row * col;
 
 			if (col == 0)
-			{
-				_putchar('0')
+                        {
+			       	_putchar('0');
+
 			}
 			else
-         
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -28,7 +28,7 @@ void times_table(void)
 					_putchar(' ');
 				}
 			}
-			
+
 			if (product >= 10)
 			{
 				_putchar(product / 10 + '0');
@@ -36,7 +36,7 @@ void times_table(void)
 			}
 			else
 			{
-                                _putchar(product + '0');
+				_putchar(product + '0');
 			}
 		}
 
