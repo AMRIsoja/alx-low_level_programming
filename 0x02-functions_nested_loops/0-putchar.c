0f7d2712b957f68e08639593cd2beba0aed52de0
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-15 17:02:53 -0700

    corrections

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
  *
  * Return: Always returns 0.
  */
-int main(void) {
+int main(void)
+{
     _putchar('_');
     _putchar('p');
     _putchar('u');
@@ -16,5 +17,5 @@ int main(void) {
     _putchar('r');
     _putchar('\n');
 
-    return 0;
+    return (0);
 }
