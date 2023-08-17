df80d3a84878023a274915412060c38db13d0ef5
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-17 15:32:50 -0700

    corrections

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,16 @@
 #define MAIN_H
 
 /**
+ * _putchar - Writes a character to the standard output (stdout).
+ *
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c);
+
+/**
  * Description: this isa function to find id digit or no
  *  
  * if it's digit  otherwise 
