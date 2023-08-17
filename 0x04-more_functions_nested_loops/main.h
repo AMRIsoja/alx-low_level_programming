e8bbe77d5dfbeb2884016640b94173f1bece7b61
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-17 14:28:35 -0700

    corrections

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 /***
  * Description: this isa function to find id digit or not
  *
  * if it's digit 0 otherwise 1
  *
  */
-
 int _isdigit(int c);
 
 #endif /* MAIN_H */
