60942f0e5813ed09b7be068ed3792fe9630b9488
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-17 17:54:23 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -42,5 +42,10 @@ int mul(int a, int b);
  */
 void print_numbers(void);
 
+/**
+ * print_most_numbers - Prints the numbers from 0 to 9 (excluding 2 and 4), followed by a new line.
+ */
+void print_most_numbers(void);
+
 #endif /* MAIN_H */
 
