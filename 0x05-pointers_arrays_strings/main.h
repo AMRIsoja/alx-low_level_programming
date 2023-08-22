21651f57f193da2ce0855596476b946f9f8ed070
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-21 20:03:58 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -76,5 +76,15 @@ void puts_half(char *str);
  */
 void print_array(int *a, int n);
 
+/**
+ * _strcpy - Copies the string pointed to by src 
+ * to the buffer pointed to by dest.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ *
+ * Return: The pointer to dest.
+ */
+char *_strcpy(char *dest, char *src);
+
 #endif /* MAIN_H */
 
