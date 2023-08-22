6610170783da9e42f1b2c7e8cc79002288531a3e
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-22 13:35:54 -0700

    corrections

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,15 +3,31 @@
 #include <time.h>
 
 /**
- * keygen password generator
+ * keygen password program
  *
- * this function generate a valid password
+ * this generate valid random password
  */
-int main(void)
-{
+char* generatePassword() {
+    char* password = (char*)malloc(7); /* 6 characters + null terminator */
+    int i;
+
     srand(time(NULL)); /* Initialize the random number generator with the current time */
 
+    for (i = 0; i < 6; i++) {
+        /* Generate a random character between 'A' and 'z' (inclusive) */
+        password[i] = rand() % ('z' - 'A' + 1) + 'A';
+    }
+    password[6] = '\0'; /* Add null terminator to create a valid string */
+
+    return password;
+}
+
+int main(void) {
+    char* password = generatePassword();
     printf("Tada! Congrats\n");
+
+    /* Don't forget to free the dynamically allocated memory */
+    free(password);
 
     return 0;
 }
