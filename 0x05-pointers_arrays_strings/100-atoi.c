1ceb16ca7de8b6fd53e715e1d697339a62ef97df
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-22 08:56:48 -0700

    corrections

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h> // Include the <limits.h> header for INT_MAX and INT_MIN
 
 /**
  * _atoi - Converts a string to an integer.
@@ -8,35 +9,37 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1; /* Default positive sign */
-	int result = 0;
+    int sign = 1; // Default positive sign
+    int result = 0;
 
-/* Skip leading whitespace */
-	while (*s == ' ')
-		s++;
+    // Skip leading whitespace
+    while (*s == ' ')
+        s++;
 
-/* Check for sign */
-	if (*s == '-' || *s == '+')
-	{
-		if (*s == '-')
-			sign = -1;
-		s++;
-	}
+    // Check for sign
+    if (*s == '-' || *s == '+')
+    {
+        if (*s == '-')
+            sign = -1;
+        s++;
+    }
 
-/* Convert digits to integer */
-	while (*s >= '0' && *s <= '9')
-	{
-/* Check for overflow */
-		if (result > (INT_MAX - (*s - '0')) / 10)
-		{
-			if (sign == 1)
-				return INT_MAX;
-			else
-				return INT_MIN;
-        	}
-		result = result * 10 + (*s - '0');
-		s++;
-	}
+    // Convert digits to integer
+    while (*s >= '0' && *s <= '9')
+    {
+        // Check for overflow
+        if (result > (INT_MAX - (*s - '0')) / 10)
+        {
+            if (sign == 1)
+                return INT_MAX;
+            else
+                return INT_MIN;
+        }
+        
+        result = result * 10 + (*s - '0');
+        s++;
+    }
 
-	return result * sign;
+    return result * sign;
 }
+
