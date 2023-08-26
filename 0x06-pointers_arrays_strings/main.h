c8c20716482d12033e755819d6bb0c7efab515f5
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-25 22:31:12 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: Pointer to the resulting string `dest`
+ */
+char *_strcat(char *dest, char *src);
+
+#endif /* MAIN_H */
+
