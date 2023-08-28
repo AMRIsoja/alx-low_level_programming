8560c63ec7aa37bbddbb8a9faf65099d06f9065b
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-28 12:10:01 -0700

    0. memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+/** Function prototype for _putchar 
+ *
+ *
+ */
+int _putchar(char c);
+
+/**
+ * _memset - Fills memory with a constant byte
+ * @s: Pointer to the memory area to be filled
+ * @b: The constant byte to fill with
+ * @n: Number of bytes to fill
+ * Return: Pointer to the filled memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
+
