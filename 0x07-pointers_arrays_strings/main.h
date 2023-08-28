64e77ea44392fe616a3c315d60922b7f0848c9a6
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-28 12:39:37 -0700

    1. memcpy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -18,5 +18,14 @@ int _putchar(char c);
  */
 char *_memset(char *s, char b, unsigned int n);
 
+/**
+ * _memcpy - Copies n bytes from memory area src to memory area dest
+ * @dest: Pointer to the destination memory area
+ * @src: Pointer to the source memory area
+ * @n: Number of bytes to copy
+ * Return: Pointer to the destination memory area
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
 #endif /* MAIN_H */
 
