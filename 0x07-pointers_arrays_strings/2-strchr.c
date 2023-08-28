44f5aec29f798d85fec123f3fa1f302e614834d9
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-28 13:05:53 -0700

    corrections

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,8 @@
  * _strchr - Locates a character in a string
  * @s: Pointer to the string to search
  * @c: Character to locate
- * Return: Pointer to the first occurrence of c in s, or NULL if not found
+ * Return: Pointer to the first occurrence of c in s, 
+ * or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -31,7 +31,8 @@ char *_memcpy(char *dest, char *src, unsigned int n);
  * _strchr - Locates a character in a string
  * @s: Pointer to the string to search
  * @c: Character to locate
- * Return: Pointer to the first occurrence of c in s, or NULL if not found
+ * Return: Pointer to the first occurrence of c in s, 
+ * or NULL if not found
  */
 char *_strchr(char *s, char c);
 
