4b950a108e188291edbca209049bd87a068ef68d
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-08-30 17:42:25 -0700

    0 puts_recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * putchar function 
+ * print character
+ * return 
+ */
 int _putchar(char c);
+
+/**
+ * _puts_recursion followed by a new line
+ * @s: the string to be prnted
+ *
+ */
 void _puts_recursion(char *s);
 
 #endif /* MAIN_H */
