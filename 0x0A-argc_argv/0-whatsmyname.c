6fd18179fb26c8dbbc9e99a3fea5b8677405adfc
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-09-06 00:26:11 -0700

    corrections

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 
-
-/**
- * this function 
- *
- * print it's name
- *
- */
 int main(int argc, char *argv[])
 {
 	if (argc > 0 && argv[0])
 		printf("%s\n", argv[0]);
-
 	return (0);
 }
 
