60a23486a89e0f91d785036b3bf4b1560c45cccf
AMRIsoja <abdulazizmuhammadrabiu@gmail.com>
2023-09-12 09:50:35 -0700

    changes

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,16 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef FUNCTION_MACROS_H
+#define FUNCTION_MACROS_H
 
-#define ABS(x) ((x) < (0) ? (x * -1) : (x))
+/*
+ * ABS(x) - Computes the absolute value of x.
+ *
+ * This macro returns the absolute value of the given number x.
+ * If x is negative, it negates it to make it positive.
+ *
+ * Parameters:
+ * x: The number for which the absolute value is calculated.
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
+
